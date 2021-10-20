c465c367149baf8a3304d8d3f82cff7404b5f41f
Максим Секачёв <cement-fan@ya.ru>
2021-10-20 19:29:38 +0300

    whith api

--- api/management/commands/load_cbr.py
+++ api/management/commands/load_cbr.py
@@ -1,56 +1,38 @@
-from decimal import Decimal
-import xml.dom.minidom as minidom
-from datetime import datetime
-
 import requests
 from django.core.management import BaseCommand
+from django.db import transaction, IntegrityError
 
 from api.models import Currency
 
 
 class Command(BaseCommand):
     def load_cbr(self):
-        url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+        url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+        data = requests.get(url).json()
+        currencies = data.get('Valute')
 
-        resp = requests.get(url)
-        doc = minidom.parseString(resp.content.decode('cp1251'))
-
-        # Date parsing
-        valcurs = doc.getElementsByTagName('ValCurs')[0]
-        date_rate = valcurs.attributes.get('Date').value
-        date_rate = datetime.strptime(date_rate, '%d.%m.%Y')
-
-        # Rate parsing
-        valutes = doc.getElementsByTagName('Valute')
-        for item in valutes:
-            code = item.attributes.get('ID').value
-            num_code = item.getElementsByTagName('NumCode')[0].childNodes[0].data
-            char_code = item.getElementsByTagName('CharCode')[0].childNodes[
-                0].data
-
-            nominal = item.getElementsByTagName('Nominal')[0].childNodes[0].data
-            name = item.getElementsByTagName('Name')[0].childNodes[0].data
-            rate = item.getElementsByTagName('Value')[0].childNodes[0].data
-
-            nominal = int(nominal)
-            rate = Decimal(rate.replace(',', '.'))
-
+        if not currencies:
+            self.stdout.write(
+                self.style.SUCCESS('Ошибка подключения'))
+            return
+        
+        for currency in currencies.values():
+            name = currency.get('Name')
+            rate = currency.get('Value')
+            nominal = currency.get('Nominal', 1)
             rub_rate = rate / nominal
 
-            currency, created = Currency.objects.get_or_create(
-                name=name,
-                rate=rub_rate
-            )
+            try:
+                with transaction.atomic():
+                    currency, created = Currency.objects.update_or_create(
+                        name=name,
+                        defaults={'rate': rub_rate},
+                    )
+            except IntegrityError as err:
+                self.stdout.write(
+                    self.style.WARNING(f'Данные для {name} не сохранены'))
 
-            # try:
-            #     change = rate - CurrencyRate.objects.filter(
-            #         currency=currency).last().rate
-            # except AttributeError:
-            #     change = 0
-
-            # course, created = CurrencyRate.objects.get_or_create(
-            #     currency=currency, date_rate=date_rate,
-            #     defaults={'nominal': nominal, 'rate': rate})
+        self.stdout.write(self.style.SUCCESS('Курс валют успешно сохранен'))
 
     def handle(self, *args, **options):
         self.load_cbr()
